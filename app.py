e180d2d65494745402ca01757d133cf8c62c76a2
aozcan <anilozcan35@gmail.com>
2024-05-04 18:40:23 +0300

    dashboardlar eklendi.

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import preporcess
+import model
+
+with st.sidebar:
+    st.write("sidebar")
+    st.button("train")
+
+st.title("Body Performance Data")
+st.markdown("Toy model and its applications")
+df = preporcess.preprocess()
+st.table(df.head())
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("#### Classification Section")
+    classification_task = model.ClassificationTask(df)
+    report, cm = classification_task.tune_and_predict_classification("naive_bayes")
+    st.dataframe(report)
+    st.pyplot(cm)
+
+with col2:
+    st.markdown("#### Clustering Section")
+    clustering_task = model.ClusteringTask(df)
+    fig_elbow = clustering_task.show_elbow()
+    st.pyplot(fig_elbow)
+    fig_clusters = clustering_task.cluster_plots()
+    st.pyplot(fig_clusters)
+
+
+
+
+
+st.markdown("#### Prediction Section")
+
+
