df6868760f292ca5394d3abc884557cf9488d320
aozcan <anilozcan35@gmail.com>
2024-05-15 01:16:24 +0300

    add prediction feature and graphs

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ from dotenv import load_dotenv
 from src.functions import download_dataset_from_kaggle
 from src.streamlit_functions import (data_metadata, data_preview,
                                      data_profiling, data_profilingA)
+from src.models import dt_algoritm, knn_algoritm, nb_algorithm, k_means
 
-from src.models.dt_algoritm import dt_train, dt_prediction, dt_model_chart, dt_model_evaluation, dt_save_model
+from src.models.dt_algoritm import dt_train, dt_prediction
 
 # from st_pages import show_pages_from_config
 
@@ -32,7 +33,8 @@ def home():
     st.markdown('**1. Project Proposal:** Proje önerisi ve proje hakkında bilgi alabilirsiniz.')
     st.markdown('**2. Project System Design:** Proje aşamaları ve sistem tasarımı hakkında bilgi alabilirsiniz.')
     st.markdown('**3. Dataset Info:** Veri seti önizlemesi yapabilirsiniz. Veri seti hakkında bilgi alabilirsiniz.')
-    st.markdown('**4. Multi Class Classification:** Çoklu sınıflandırma uygulamasıdır. Veri seti üzerinde çoklu sınıflandırma yapabilirsiniz.')
+    st.markdown(
+        '**4. Multi Class Classification:** Çoklu sınıflandırma uygulamasıdır. Veri seti üzerinde çoklu sınıflandırma yapabilirsiniz.')
     st.markdown('**5. Regression:** Regresyon uygulamasıdır. Veri seti üzerinde regresyon analizi yapabilirsiniz.')
     st.markdown('**6. Clustering:** Kümeleme uygulamasıdır. Veri seti üzerinde kümeleme analizi yapabilirsiniz.')
     st.markdown('**7. App Info. & Credits:** Bu projede kullanılan Framework ve Libraryleri içermektedir.')
@@ -50,9 +52,9 @@ def pipeline():
     st.title('Project System Design Title')
     st.header('Project System Design Header')
     st.subheader('Proje Sistemi Tasarımı: Proje aşamaları ve sistem tasarımı hakkında bilgi alabilirsiniz.')
-    st.image(image="./pipeline/SystemDesign.jpg", 
-             caption="Project System Design", 
-             width=200, 
+    st.image(image="./pipeline/SystemDesign.jpg",
+             caption="Project System Design",
+             width=200,
              use_column_width="auto"
              )
 
@@ -65,11 +67,12 @@ def data():
     st.subheader('Veri seti önizlemesi yapabilirsiniz. Veri seti hakkında bilgi alabilirsiniz.')
     tab1, tab2, tab3, tab4 = st.tabs(["Meta Data", "Preview", "Profile(Raw Data)", "Profile(Preprocess Data)"])
     with tab1:
-        st.image(image="https://storage.googleapis.com/kaggle-datasets-images/1732554/2832282/1be2ae7e0f1bc3983e65c76bfe3a436e/dataset-cover.jpg?t=2021-11-20-09-31-54",
-                 caption="Body Performance Dataset from Kaggle",
-                 width=200,
-                 use_column_width="auto"
-                 )
+        st.image(
+            image="https://storage.googleapis.com/kaggle-datasets-images/1732554/2832282/1be2ae7e0f1bc3983e65c76bfe3a436e/dataset-cover.jpg?t=2021-11-20-09-31-54",
+            caption="Body Performance Dataset from Kaggle",
+            width=200,
+            use_column_width="auto"
+        )
         st.title('Meta Data')
         st.header("Meta Data")
         data_metadata(file_path=DATA_FILE)
@@ -90,6 +93,34 @@ def data():
             components.html(p.read(), height=4096, width=2160, scrolling=True)
 
 
+def get_prediction_records(key_start=0):
+    age = st.text_input(label="Age", value=26.0, key=key_start)
+    gender = st.text_input(label="Gender", value="M", key=key_start + 1)
+    height_cm = st.text_input(label="HeightCm", value=170, key=key_start + 2)
+    weight_kg = st.text_input(label="WeightKg", value=55.8, key=key_start + 3)
+    fat = st.text_input(label="Fat", value=15.7, key=key_start + 4)
+    diastolic = st.text_input(label="Diastolic", value=77.0, key=key_start + 5)
+    systolic = st.text_input(label="Systolic", value=126.0, key=key_start + 6)
+    gripForce = st.text_input(label="GripForce", value=36.4, key=key_start + 7)
+    forward_cm = st.text_input(label="ForwardCm", value=16.3, key=key_start + 8)
+    sit_ups = st.text_input(label="SitUps", value=53.0, key=key_start + 9)
+    jump_cm = st.text_input(label="JumpCm", value=29.0, key=key_start + 10)
+    classs = "A"
+    prediction_record = [float(age),
+                         gender,
+                         float(height_cm),
+                         float(weight_kg),
+                         float(fat),
+                         float(diastolic),
+                         float(systolic),
+                         float(gripForce),
+                         float(forward_cm),
+                         float(sit_ups),
+                         float(jump_cm),
+                         classs]
+    return prediction_record
+
+
 def classification():
     """Multi Class Classification Page"""
 
@@ -103,76 +134,143 @@ def classification():
     #                       )
     # st.write("Model Seçimi:", option)
 
-    tab1, tab2, tab3, tab4 = st.tabs(["Decision Tree (DT)", "K Nearest Neighboor (KNN)", "Light GBM", "CatBoost"])
+    tab1, tab2, tab3 = st.tabs(["Decision Tree (DT)", "K Nearest Neighbor (KNN)", "Naive Bayes"])
     with tab1:
         st.header("Decision Tree")
         st.write('Decision Tree işlemi yapılacak.')
 
-        tab1_1, tab1_2, tab1_3, tab1_4 = st.tabs(["Training Component Component", "Model Charts", "Prediction Component", "Other"])
+        tab1_1, tab1_2, tab1_3, tab1_4 = st.tabs(
+            ["Training Component Component", "Model Charts", "Prediction Component", "Other"])
         with tab1_1:
             st.header("Decision Tree Training Component")
-            st.write('Decision Tree işlemi yapılacak.')
-            dt_train(df_test=None, target_column=None, model_name=None, model_params=None)
-            
-        with tab1_2:
-            st.header("Decision Tree Model Charts")
-            st.write('Decision Tree işlemi yapılacak.')
-            dt_model_chart(model_name=None, model_params=None, df_test=None, target_column=None)
-            
-        with tab1_3:
-            st.header("Decision Tree Pre")
-            st.write('Decision Tree işlemi yapılacak.')
-            dt_prediction(df_test=None, model_name=None, model_params=None)
+            st.write('Decision Tree Training Process Will Be Done.')
+            dt_train_button = st.button("Train DT")
 
-        with tab1_4:
-            st.header("Decision Tree Other")
-            st.write('Decision Tree işlemi yapılacak.')
-            dt_model_evaluation(model_name=None, model_params=None, df_test=None, target_column=None)
+            if dt_train_button:
+                classification_report, cm = dt_algoritm.dt_train(model_name="decision_tree")
+
+                with tab1_2:
+                    st.header("Decision Tree Model Charts")
+                    st.pyplot(cm)
+
+                with tab1_4:
+                    st.header("Decision Tree Other")
+                    st.dataframe(classification_report)
+
+            with tab1_3:
+                st.header("Decision Tree Prediction")
+                st.write('Decision Tree Prediction Will Be Done.')
+                prediction_record = get_prediction_records(key_start=0)
+                print(prediction_record)
+                predict_clicked = st.button("Predict", key = 100)
+
+                if predict_clicked:
+                    pred = dt_algoritm.dt_prediction(prediction_record)
+                    st.header("Prediction")
+                    st.write(pred)
+
+                    with tab1_1:
+                        classification_report, cm = dt_algoritm.dt_train(model_name="decision_tree")
+
+                    with tab1_2:
+                        st.header("Decision Tree Model Charts")
+                        st.pyplot(cm)
+
+                    with tab1_4:
+                        st.header("Decision Tree Other")
+                        st.dataframe(classification_report)
 
     with tab2:
-        st.header("K Nearest Neighboor")
-        st.write('K Nearest Neighboor işlemi yapılacak.')
+        st.header("K Nearest Neighbor")
+        st.write("K Nearest Neighbor Process Will Be Done.")
 
-        tab2_1, tab2_2, tab2_3 = st.tabs(["Training Component", "Model Charts", "Prediction Component"])
+        tab2_1, tab2_2, tab2_3, tab2_4 = st.tabs(
+            ["Training Component Component", "Model Charts", "Prediction Component", "Other"])
         with tab2_1:
-            st.header("K Nearest Neighboor Training Component")
-            st.write('K Nearest Neighboor işlemi yapılacak.')
-        with tab2_2:
-            st.header("K Nearest Neighboor Model Charts")
-            st.write('K Nearest Neighboor işlemi yapılacak.')
-        with tab2_3:
-            st.header("K Nearest Neighboor Predection")
-            st.write('K Nearest Neighboor işlemi yapılacak.')
+            st.header("K Nearest Neighbor Training Component")
+            st.write('K Nearest Neighbor Process Will Be Done.')
+            knn_train_button = st.button("Train KNN")
+
+            if knn_train_button:
+                classification_report, cm = knn_algoritm.knn_train(model_name="knn")
+
+                with tab2_2:
+                    st.header("K Nearest Neighbor Model Charts")
+                    st.pyplot(cm)
+
+                with tab2_4:
+                    st.header("K Nearest Neighbor Other")
+                    st.dataframe(classification_report)
+
+            with tab2_3:
+                st.header("K Nearest Neighbor Prediction")
+                st.write("K Nearest Neighbor Process Will Be Done.")
+                prediction_record = get_prediction_records(key_start=11)
+                print(prediction_record)
+                knn_predict_clicked = st.button("Predict", key=101)
+
+                if knn_predict_clicked:
+                    pred = knn_algoritm.knn_prediction(prediction_record)
+                    st.header("Prediction")
+                    st.write(pred)
+
+                    with tab2_1:
+                        classification_report, cm = knn_algoritm.knn_train(model_name="knn")
+
+                    with tab2_2:
+                        st.header("K Nearest Neighbor Model Charts")
+                        st.pyplot(cm)
+
+                    with tab2_4:
+                        st.header("Other")
+                        st.dataframe(classification_report)
 
     with tab3:
-        st.header("Light GBM")
-        st.write('Light GBM işlemi yapılacak.')
+        st.header("Naive Bayes")
+        st.write("Naive Bayes Process Will Be Done.")
 
-        tab3_1, tab3_2, tab3_3 = st.tabs(["Training Component", "Model Charts", "Prediction Component"])
+        tab3_1, tab3_2, tab3_3, tab3_4 = st.tabs(
+            ["Training Component Component", "Model Charts", "Prediction Component", "Other"])
         with tab3_1:
-            st.header("Light GBM Training Component")
-            st.write('Light GBM işlemi yapılacak.')
-        with tab3_2:
-            st.header("Light GBM Model Charts")
-            st.write('Light GBM işlemi yapılacak.')
-        with tab3_3:
-            st.header("Light GBM Prediction Component")
-            st.write('Light GBM işlemi yapılacak.')
+            st.header("Naive Bayes Training Component")
+            st.write('Naive Bayes Training Process Will Be Done.')
+            nb_train_button = st.button("Train NB", key=103)
 
-    with tab4:
-        st.header("CatBoost")
-        st.write('CatBoost işlemi yapılacak.')
+            if nb_train_button:
+                classification_report, cm = nb_algorithm.nb_train(model_name="naive_bayes")
 
-        tab4_1, tab4_2, tab4_3 = st.tabs(["Training Component", "Model Charts", "Prediction Component"])
-        with tab4_1:
-            st.header("CatBoost Training Component")
-            st.write('CatBoost işlemi yapılacak.')
-        with tab4_2:
-            st.header("CatBoost Model Charts")
-            st.write('CatBoost işlemi yapılacak.')
-        with tab4_3:
-            st.header("CatBoost Prediction Component")
-            st.write('CatBoost işlemi yapılacak.')
+                with tab3_2:
+                    st.header("Naive Bayes Model Charts")
+                    st.pyplot(cm)
+
+                with tab3_4:
+                    st.header("Other")
+                    st.dataframe(classification_report)
+
+            with tab3_3:
+                st.header("Naive Bayes Prediction")
+                st.write("Naive Bayes Prediction Process Will Be Done.")
+                prediction_record = get_prediction_records(key_start=22)
+                print(prediction_record)
+                nb_predict_clicked = st.button("Predict", key=104)
+
+                if nb_predict_clicked:
+                    pred = nb_algorithm.nb_prediction(prediction_record)
+                    st.header("Prediction")
+                    st.write(pred)
+
+                    with tab3_1:
+                        classification_report, cm = nb_algorithm.nb_train(model_name="naive_bayes")
+
+                    with tab3_2:
+                        st.header("Naive Bayes Model Charts")
+                        st.pyplot(cm)
+
+                    with tab3_4:
+                        st.header("Other")
+                        st.dataframe(classification_report)
+
+
 
 def regression():
     """Regression Page"""
@@ -205,10 +303,49 @@ def regression():
 def clustering():
     """Clustering Page"""
 
-    st.title('Clustering Title')
-    st.header('Clustering Algorithms Header')
-    st.subheader('Kümeleme uygulamasıdır. Veri seti üzerinde kümeleme analizi yapabilirsiniz.')
-    # st.write('1. Veri seti yükleme')
+    st.title('Clustering Section')
+    st.header('K Means Algorithm')
+    st.subheader('Clustering jobs could be executed in this section.')
+    tab1, tab2, tab3, tab4 = st.tabs(
+        ["Selection Training Param", "Elbow Graph", "Clusters", "Prediction Section"])
+
+    with tab1:
+        st.header("Training Component")
+        st.write('Selection Training Param')
+        selected_n_cluster = st.slider("Set K", min_value=2, max_value=6, value=4)
+        show_clicked = st.button("Train K-means", key=106)
+        if show_clicked:
+            fig_elbow, fig_clusters = k_means.k_means_train(selected_n_cluster=selected_n_cluster)
+            with tab2:
+                st.subheader("Elbow Graph")
+                st.write('K Params Optimal for 4')
+                st.pyplot(fig_elbow)
+            with tab3:
+                st.subheader("Clusters")
+                st.pyplot(fig_clusters)
+
+        with tab4:
+            st.header("K-Means Prediction")
+            st.write("K-Means Prediction Process Will Be Done.")
+            prediction_record = get_prediction_records(key_start=33)
+            print(prediction_record)
+            cluster_predict = st.button("Predict", key=105)
+
+            if cluster_predict:
+                fig = k_means.k_means_predict(prediction_record, selected_n_cluster)
+                st.header("Prediction of Point")
+                st.pyplot(fig)
+
+                fig_elbow, fig_clusters = k_means.k_means_train(selected_n_cluster=selected_n_cluster)
+                with tab2:
+                    st.subheader("Elbow Graph")
+                    st.write('K Params Optimal for 4')
+                    st.pyplot(fig_elbow)
+                with tab3:
+                    st.subheader("Clusters")
+                    st.pyplot(fig_clusters)
+                    # Buralardaki kod tekrarnı nasıl çözülebilir.
+
 
 
 def credits():
@@ -271,7 +408,6 @@ def menu(user_name=None, user_password=None):
 
 
 if __name__ == "__main__":
-
     # Set Constants
     ROOT_PATH = os.getcwd()
     CFG_PATH = os.path.join(ROOT_PATH, 'cfg')
@@ -287,8 +423,8 @@ if __name__ == "__main__":
     load_dotenv(dotenv_path=ENV, encoding='utf-8', verbose=False)
 
     # Get Constants
-    USER_NAME = os.environ.get("USER_NAME")
-    USER_PASSWORD = os.environ.get("USER_PASSWORD")
+    #USER_NAME = os.environ.get("USER_NAME")
+    #USER_PASSWORD = os.environ.get("USER_PASSWORD")
 
     st.set_page_config(
         page_title="Web Mining Project UI ",
@@ -302,11 +438,12 @@ if __name__ == "__main__":
                     'Report a bug': "https://www.extremelycoolapp.com/bug",
                     'About': "# This is a header. This is an *extremely* cool app!"
                     }
-                    )
+    )
 
     download_dataset_from_kaggle(user_name="kukuroo3", dataset_name="body-performance-data", path=RAW_DATA_PATH)
     # data_profiling(file_path=DATA_FILE, report_path=PROFILLING_PATH, minimal=False)
-    data_profilingA(file_path=DATA_FILE, report_path=PROFILLING_PATH, minimal=False, report_file_name="RawDataProfilingReport")
-    data_profilingA(file_path=DATA_FILE, report_path=PROFILLING_PATH, minimal=False, report_file_name="PreprocessDataProfilingReport")
-    menu(user_name=USER_NAME, user_password=USER_PASSWORD)
-
+    data_profilingA(file_path=DATA_FILE, report_path=PROFILLING_PATH, minimal=False,
+                    report_file_name="RawDataProfilingReport")
+    data_profilingA(file_path=DATA_FILE, report_path=PROFILLING_PATH, minimal=False,
+                    report_file_name="PreprocessDataProfilingReport")
+    menu(user_name='local', user_password='local')  # login için değiştirildi.
